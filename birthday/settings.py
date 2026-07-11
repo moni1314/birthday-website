f1c57d83d315a7552852722e6002a5c9e4a4d81d
Moni <prabhavathithangaraj2007@gmail.com>
2026-07-11 18:30:56 +0530

    Fix middleware syntax

--- birthday/settings.py
+++ birthday/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'birthday.urls'
 
 TEMPLATES = [
